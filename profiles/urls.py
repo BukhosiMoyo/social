1b8bb529b8da3b426bdb53e6d4464fd28fd27f6a
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-08-09 21:04:37 +0200

    ✨ : Adding the invitations urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, invites_received_view
 
 app_name = 'profiles'
 
 urlpatterns = [
     path("my-profile/", ProfileView, name="profile"),
+    path("invitations/", invites_received_view, name="invitations"),
    
 ]
