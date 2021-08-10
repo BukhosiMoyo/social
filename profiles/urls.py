25586608c572a37b70c3eefac7870b9be1070713
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-08-10 16:16:51 +0200

    ✨ : Adding all profiles and to invite to the urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProfileView, invites_received_view
-
+from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path("my-profile/", ProfileView, name="profile"),
-    path("invitations/", invites_received_view, name="invitations"),
+    path("my-profile/", views.ProfileView, name="profile"),
+    path("invitations/", views.invites_received_view, name="invitations"),
+    path("all-profiles/", views.profiles_list_view, name="profile-list"),
+    path("to-invite/", views.invite_profiles_list_view, name="profile-list"),
    
 ]
